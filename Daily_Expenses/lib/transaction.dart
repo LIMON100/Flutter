a6861d7f3895e6610153a3badadf1065005d568a
limon100 <mahmudurlimon41@gmail.com>
2021-06-29 18:36:23 +0600

    transaction

---
+++ Daily_Expenses/lib/transaction.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({this.id, this.title, this.amount, this.date});
+}
