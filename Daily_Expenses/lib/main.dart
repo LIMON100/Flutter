82025e89c36c950035d83e65d9e668904bd064b3
limon100 <mahmudurlimon41@gmail.com>
2021-06-30 18:21:05 +0600

    little styling

--- Daily_Expenses/lib/main.dart
+++ Daily_Expenses/lib/main.dart
@@ -41,7 +41,7 @@ class MyHomePage extends StatelessWidget {
           Container(
             width: double.infinity,
             child: Card(
-              color: Colors.yellow,
+              color: Colors.red,
               child: Text('Chart!'),
               elevation: 5,
             ),
@@ -52,13 +52,35 @@ class MyHomePage extends StatelessWidget {
               child: Row(
                 children: <Widget>[
                   Container(
-                    child: Text(tx.amount.toString()),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2),
+                    ),
+                    padding: EdgeInsets.all(12),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(tx.title),
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                       Text(
                         tx.date.toString(),
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
                       ),
                     ],
                   ),
