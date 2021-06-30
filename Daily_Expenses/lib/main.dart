a8dbe0d2081a4f1e50481c5681b5c45077e8906a
limon100 <mahmudurlimon41@gmail.com>
2021-07-01 03:13:04 +0600

    update main

--- Daily_Expenses/lib/main.dart
+++ Daily_Expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
+
+import './widgets/user_transactions.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,80 +15,30 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
-    Transaction(
-      id: 'p1',
-      title: 'Shoes',
-      amount: 969.4,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 'p2',
-      title: 'Bags',
-      amount: 19.4,
-      date: DateTime.now(),
-    ),
-  ];
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Daily Routine Apps'),
+        title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(
-              color: Colors.red,
-              child: Text('Chart!'),
+              color: Colors.blue,
+              child: Text('CHART!'),
               elevation: 5,
             ),
           ),
-          Column(
-              children: transaction.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2),
-                    ),
-                    padding: EdgeInsets.all(12),
-                    child: Text(
-                      tx.amount.toString(),
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        tx.date.toString(),
-                        style: TextStyle(
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList()),
+          UserTransactions()
         ],
       ),
     );
