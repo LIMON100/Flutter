86c29045ea65c94dbb36f070a959e066a8e8ec4f
limon100 <mahmudurlimon41@gmail.com>
2021-07-11 21:25:30 +0600

    uodate

--- Different_Meals/lib/main.dart
+++ Different_Meals/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:DelMeals/category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 import './categories_screen.dart';
+import './category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,6 +30,9 @@ class MyApp extends StatelessWidget {
             )),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/categories-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
