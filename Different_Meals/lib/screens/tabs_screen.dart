4b9166cc425bc91c32f6d844d5a98bef2d0109d7
limon100 <mahmudurlimon41@gmail.com>
2021-07-25 02:49:17 +0600

    update tab_screen

--- Different_Meals/lib/screens/tabs_screen.dart
+++ Different_Meals/lib/screens/tabs_screen.dart
@@ -41,7 +41,7 @@ class _TabsScreenState extends State<TabsScreen> {
         unselectedItemColor: Colors.white,
         selectedItemColor: Theme.of(context).accentColor,
         currentIndex: _selectedPageIndex,
-        //type: BottomNavigationBarType.shifting,
+        //type: BottomNavigationBar Type.shifting,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.category),
