1d2ffe68a18f22eee1531a8ccdd35878210e1f18
limon100 <mahmudurlimon41@gmail.com>
2021-08-01 17:50:52 +0600

    shopapp

---
+++ Simple_shop_App/android/app/src/main/kotlin/com/example/Simple_shop_App/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Simple_shop_App
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Simple_shop_App/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:Simple_shop_App/screens/product_overview_screen.dart';
+import 'package:flutter/material.dart';
+import './screens/product_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My shop",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
