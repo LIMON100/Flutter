5b8f10752bcac25fc364e26fbd1cecd927b13496
limon100 <mahmudurlimon41@gmail.com>
2021-09-04 23:57:19 +0600

    budget app

---
+++ Budget_App/lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Simple Budget"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
