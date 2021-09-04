63c5c2f42a370d541c90ff3184718bc86ab13e63
limon100 <mahmudurlimon41@gmail.com>
2021-09-05 00:22:30 +0600

    update home screen

--- Budget_App/lib/screens/home_screen.dart
+++ Budget_App/lib/screens/home_screen.dart
@@ -12,6 +12,10 @@ class _HomeScreenState extends State<HomeScreen> {
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
+            pinned: true,
+            forceElevated: true,
+            floating: true,
+            expandedHeight: 100.0,
             leading: IconButton(
               icon: Icon(Icons.settings),
               iconSize: 30.0,
@@ -20,6 +24,23 @@ class _HomeScreenState extends State<HomeScreen> {
             flexibleSpace: FlexibleSpaceBar(
               title: Text("Simple Budget"),
             ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          SliverList(
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Container(
+                margin: EdgeInsets.all(10.0),
+                height: 100.0,
+                color: Colors.red,
+              );
+            }),
           ),
         ],
       ),
