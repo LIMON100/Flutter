6623c8fa6051d4614c67180054af66ab2d3d495e
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-02-23 21:06:56 +0600

    icons

---
+++ Qr_Code_Scan_Sidebar/lib/db_icons.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DBIcons {
+  DBIcons._();
+
+  static const _kFontFam = 'DBIcons';
+  static const String? _kFontPkg = null;
+
+
+  // static const IconData logo = IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData logo = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
+
+
+// import 'package:flutter/widgets.dart';
+//
+// class Settiings {
+//   Settiings._();
+//
+//   static const _kFontFam = 'Settiings';
+//   static const String? _kFontPkg = null;
+//
+//   static const IconData cog = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+// }
