6461c7ffc8534d32bbdee7da64e291782351081e
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-03-30 17:45:36 +0600

    wifi model

---
+++ Qr_Code_Scan_Sidebar/lib/Models/model.dart
@@ -0,0 +1,33 @@
+/// ssid : "Efty"
+/// pass : "password"
+
+class Model {
+  Model({
+      String? ssid, 
+      String? pass,}){
+    _ssid = ssid;
+    _pass = pass;
+}
+
+  Model.fromJson(dynamic json) {
+    _ssid = json['ssid'];
+    _pass = json['pass'];
+  }
+  String? _ssid;
+  String? _pass;
+Model copyWith({  String? ssid,
+  String? pass,
+}) => Model(  ssid: ssid ?? _ssid,
+  pass: pass ?? _pass,
+);
+  String? get ssid => _ssid;
+  String? get pass => _pass;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['ssid'] = _ssid;
+    map['pass'] = _pass;
+    return map;
+  }
+
+}
