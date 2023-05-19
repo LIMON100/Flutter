88822637ca9cee10f1cd2b3bd977eecf26347209
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-05-19 12:13:00 +0600

    add send data

--- llamaRadar_test/lib/temp/CollisionWarningPage2.dart
+++ llamaRadar_test/lib/temp/CollisionWarningPage2.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'warning_icons.dart';
@@ -20,17 +22,6 @@ class CollisionWarningPage2 extends StatefulWidget {
 
 class _CollisionWarningPage2State extends State<CollisionWarningPage2> {
   late Timer _timer;
-  int _leftCollisionValue = 0;
-  int _rightCollisionValue = 100;
-  int _topCollisionValue = 0;
-  int _bottomCollisionValue = 101;
-  bool _blinkLeft = false;
-  bool _blinkRight = false;
-  bool _blinkTop = false;
-  bool _blinkBottom = false;
-
-
-  Timer? _blinkTimer;
   bool _isLeftBlinking = false;
   bool _isRightBlinking = false;
   bool _isTopBlinking = false;
@@ -84,8 +75,10 @@ class _CollisionWarningPage2State extends State<CollisionWarningPage2> {
   //notificaiton variable
   late BluetoothDevice _device;
   BluetoothCharacteristic? _characteristic;
+  BluetoothCharacteristic? _characteristic_write;
   Stream<List<int>>? _stream;
   String _value = '';
+  bool isDataMatched = false;
 
 
   @override
@@ -102,28 +95,86 @@ class _CollisionWarningPage2State extends State<CollisionWarningPage2> {
     for (BluetoothService service in services) {
       List<BluetoothCharacteristic> characteristics = await service.characteristics;
       for (BluetoothCharacteristic characteristic in characteristics) {
-        if (characteristic.uuid.toString() == 'beb5483e-36e1-4688-b7f5-ea07361b26a8') { // Replace with the characteristic UUID for your device
-          // _service = service;
-          _characteristic = characteristic;
+        print("Check the properties");
 
-          // Enable notifications for the characteristic
-          await _characteristic!.setNotifyValue(true);
+        if (characteristic.uuid.toString() ==
+            'beb5483e-36e1-4688-b7f5-ea07361b26a7') {
+          _characteristic_write = characteristic;
+          // break;
+        }
 
-          // Listen to the characteristic notifications
-          _characteristic!.value.listen((value) {
-            setState(() {
-              _value = value.toString();
+        if (characteristic.properties.notify){
+          if (characteristic.uuid.toString() ==
+              'beb5483e-36e1-4688-b7f5-ea07361b26a8') { // Replace with the characteristic UUID for your device
+            // _service = service;
+            _characteristic = characteristic;
+
+            // Enable notifications for the characteristic
+            await _characteristic!.setNotifyValue(true);
+
+            // Listen to the characteristic notifications
+            _characteristic!.value.listen((value) {
+              setState(() {
+                _value = value.toString();
+              });
             });
-          });
 
-          print('Found characteristic ${characteristic.uuid}');
-          break;
+            print('Found characteristic ${characteristic.uuid}');
+            // break;
+          }
         }
       }
     }
     setState(() {
       _device = widget.device;
     });
+  }
+
+  // Send data part
+  // Future<void> _sendData() async {
+  //   if (_characteristic_write != null) {
+  //     // final dataToSend = [0x02, 0x01, 0x10, 0xA, 0x02, 0x15, 0xF];
+  //     final dataToSend = [0x02, 0x01, 0xC, 0x01, 0x10];
+  //     // await characteristic!.write(dataToSend);
+  //     await _characteristic_write!.write(utf8.encode(dataToSend.toString()));
+  //     // Wait for the response from the BLE device
+  //     final response = await _characteristic_write!.value.first;
+  //     if (response.isNotEmp
+  //     ty && response[0] == 0x01) {
+  //       setState(() {
+  //         isDataMatched = true;
+  //       });
+  //     }
+  //   }
+  // }
+
+  // Future<void> _sendData() async {
+  //   if (characteristic != null) {
+  //     final dataToSend = [0x02, 0x01, 0xD, 0x01, 0x11];
+  //     List<int> byteData = utf8.encode(dataToSend.toString());
+  //     await characteristic!.write(byteData);
+  //     // Wait for the response from the BLE device
+  //     final response = await characteristic!.value.first;
+  //   }
+  // }
+
+  //BYTE ARRAY
+  // Future<void> _sendData(List<int> dataToSend) async {
+  //   if (_characteristic_write != null) {
+  //     List<int> byteData = dataToSend;
+  //     await _characteristic_write!.write(byteData);
+  //     // Wait for the response from the BLE device
+  //     final response = await _characteristic_write!.value.first;
+  //   }
+  // }
+
+  Future<void> _sendData(List<int> dataToSend) async {
+    if (_characteristic_write != null) {
+      Uint8List byteData = Uint8List.fromList(dataToSend);
+      await _characteristic_write!.write(byteData);
+      // Wait for the response from the BLE device
+      final response = await _characteristic_write!.value.first;
+    }
   }
 
   final _random = Random();
@@ -222,10 +273,10 @@ class _CollisionWarningPage2State extends State<CollisionWarningPage2> {
   }
 
   String _getLocation() {
-    if (_value.length < 12) {
+    if (_value.length < 29) {
       return 'Notification Not Available';
     }
-    switch (int.parse(_value[11])) {
+    switch (int.parse(_value[28])) {
       case 1:
         return 'Right Notification Warning';
       case 2:
@@ -242,10 +293,10 @@ class _CollisionWarningPage2State extends State<CollisionWarningPage2> {
   }
 
   Color _getColor() {
-    if (_value.isEmpty || _value.length < 12) {
+    if (_value.isEmpty || _value.length < 29) {
       return Colors.black;
     }
-    switch (int.parse(_value[11])) {
+    switch (int.parse(_value[28])) {
       case 1:
       case 3:
         return _getLocation() == 'Right Notification Warning'
@@ -373,6 +424,7 @@ class _CollisionWarningPage2State extends State<CollisionWarningPage2> {
     );
   }
 
+  final _writeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -397,174 +449,171 @@ class _CollisionWarningPage2State extends State<CollisionWarningPage2> {
             ),
           ),
         ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              // Top warning+indicator
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      _startLeftBlinking();
-                    },
-                    icon: Icon(
-                      Indicator.image2vector,
-                      size: 48,
-                      color: _isLeftBlinking ? Colors.orange : Colors.black,
-                    ),
-                  ),
-                  // BlinkingIconButton(icon: Indicator.image2vector, size: 48),
-
-                  SizedBox(width: 60),
-                  Icon(
-                    Warning.image2vector3,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            // Top warning+indicator
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    _startLeftBlinking();
+                  },
+                  icon: Icon(
+                    Indicator.image2vector,
                     size: 48,
-                    color: _isTopBlinking ? Colors.red : Colors.green,
+                    color: _isLeftBlinking ? Colors.orange : Colors.black,
                   ),
-                  SizedBox(width: 60),
+                ),
+                // BlinkingIconButton(icon: Indicator.image2vector, size: 48),
 
-                  IconButton(
-                    onPressed: () {
-                      _startRightBlinking();
-                    },
-                    icon: Icon(
-                      Indicator.image2vector__1_,
-                      size: 48,
-                      color: _isRightBlinking  ? Colors.orange : Colors.black,
-                    ),
+                SizedBox(width: 60),
+                Icon(
+                  Warning.image2vector3,
+                  size: 48,
+                  color: _isTopBlinking ? Colors.red : Colors.green,
+                ),
+                SizedBox(width: 60),
+
+                IconButton(
+                  onPressed: () {
+                    _startRightBlinking();
+                  },
+                  icon: Icon(
+                    Indicator.image2vector__1_,
+                    size: 48,
+                    color: _isRightBlinking  ? Colors.orange : Colors.black,
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
 
-              // left+right warning
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _getLeftIcon(),
-                  SizedBox(width: 15),
-                  Text(
-                    _getLocation(),
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(width: 20),
-                  _getRightIcon(),
-                ],
-              ),
+            // left+right warning
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _getLeftIcon(),
+                SizedBox(width: 15),
+                Text(
+                  _getLocation(),
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(width: 20),
+                _getRightIcon(),
+              ],
+            ),
 
-              // ligts
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                      icon: Icon(_lightOn1 ? Icons.lightbulb : Icons.lightbulb_outline),
-                      onPressed: () {
-                        setState(() {
-                          _lightOn1 = !_lightOn1;
-                        });
-                      }
-                  ),
-                  SizedBox(width: 140),
-                  IconButton(
-                      icon: Icon(_lightOn2 ? Icons.lightbulb : Icons.lightbulb_outline),
-                      onPressed: () {
-                        setState(() {
-                          _lightOn2 = !_lightOn2;
-                        });
-                      }
-                  ),
-                ],
-              ),
-
-              // Rear warning
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _getRearIcon(),
-                ],
-              ),
-
-              //other buttons
-              SizedBox(width: 1),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Icon(_cameraOn ? Icons.camera_alt : Icons.camera_alt_outlined),
+            // ligts
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    icon: Icon(_lightOn1 ? Icons.lightbulb : Icons.lightbulb_outline),
                     onPressed: () {
                       setState(() {
-                        _cameraOn = !_cameraOn;
+                        _lightOn1 = !_lightOn1;
                       });
-                      // open camera if _cameraOn is true
-                    },
-                  ),
-                  SizedBox(width: 20),
-                  IconButton(
-                    icon: Icon(_emergencyOn ? Icons.emergency_sharp : Icons.emergency_outlined),
-                    onPressed: () {
-                      _startBothBlinking();
-                    },
-                  ),
-                  SizedBox(width: 20),
-                  IconButton(
-                      icon: Icon(_lightOn1 ? Icons.lightbulb : Icons.lightbulb_outline),
-                      onPressed: () {
-                        setState(() {
-                          _lightOn1 = !_lightOn1;
-                        });
-                      }
-                  ),
-                ],
-              ),
-
-              // Notificaiton
-
-              // Column(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     SizedBox(height: 16),
-              //     Row(
-              //       mainAxisAlignment: MainAxisAlignment.center,
-              //       children: [
-              //         _getLeftIcon(),
-              //         SizedBox(width: 16),
-              //         Text(
-              //           _getLocation(),
-              //           style: TextStyle(fontSize: 20),
-              //         ),
-              //         SizedBox(width: 16),
-              //         _getRightIcon(),
-              //       ],
-              //     ),
-              //     SizedBox(width: 2),
-              //     _getRearIcon(),
-              //   ],
-              // ),
-
-              // Image with power button
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 120),
-                  Image.asset(
-                    'images/llama_img.png',
-                    height: 100,
-                    width: 130,
-                  ),
-                  SizedBox(width: 16),
-                  FloatingActionButton(
+                    }
+                ),
+                SizedBox(width: 140),
+                IconButton(
+                    icon: Icon(_lightOn2 ? Icons.lightbulb : Icons.lightbulb_outline),
                     onPressed: () {
                       setState(() {
-                        _powerOn = !_powerOn;
+                        _lightOn2 = !_lightOn2;
                       });
-                    },
-                    backgroundColor: _powerOn ? Colors.red : Colors.blueGrey,
-                    child: Icon(Icons.power_settings_new),
-                  ),
-                ],
-              )
-            ],
-          ),
+                    }
+                ),
+              ],
+            ),
+
+            // Rear warning
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _getRearIcon(),
+              ],
+            ),
+
+            //other buttons
+            SizedBox(width: 1),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(_cameraOn ? Icons.camera_alt : Icons.camera_alt_outlined),
+                  onPressed: () {
+                    setState(() {
+                      _cameraOn = !_cameraOn;
+                    });
+                    // open camera if _cameraOn is true
+                  },
+                ),
+                SizedBox(width: 20),
+                IconButton(
+                  icon: Icon(_emergencyOn ? Icons.emergency_sharp : Icons.emergency_outlined),
+                  onPressed: () {
+                    _startBothBlinking();
+                  },
+                ),
+                SizedBox(width: 20),
+                IconButton(
+                    icon: Icon(_lightOn1 ? Icons.lightbulb : Icons.lightbulb_outline),
+                    onPressed: () {
+                      setState(() {
+                        _lightOn1 = !_lightOn1;
+                      });
+                    }
+                ),
+              ],
+            ),
+
+            // test data send
+
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                ElevatedButton(
+                  onPressed: () {
+                    _sendData([0x02, 0x01, 0xD, 0x01, 0x11]);
+                  },
+                  // onPressed: showInfo,
+                  child: const Text('Right button pressed'),
+                ),
+                const SizedBox(height: 16),
+                if (isDataMatched)
+                  const Text('Data matched')
+                else
+                  const Text('Data not matched'),
+              ],
+            ),
+
+            // Image with power button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 120),
+                Image.asset(
+                  'images/llama_img.png',
+                  height: 100,
+                  width: 130,
+                ),
+                SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      _powerOn = !_powerOn;
+                    });
+                  },
+                  backgroundColor: _powerOn ? Colors.red : Colors.blueGrey,
+                  child: Icon(Icons.power_settings_new),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
