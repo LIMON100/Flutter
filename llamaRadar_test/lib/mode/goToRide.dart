50dbe914da3c67a652e79b5002c0c33c7c48874d
LIMON100 <mahmudurlimon41@gmail.com>
2023-05-22 12:14:11 +0600

    add friles from tst

--- llamaRadar_test/lib/mode/goToRide.dart
+++ llamaRadar_test/lib/mode/goToRide.dart
@@ -18,57 +18,38 @@ class GoToRide extends StatefulWidget {
 
 class _GoToRideState extends State<GoToRide> {
   BluetoothDevice? _connectedDevice;
-  bool _isConnected = false;
+  bool _isDisconnected = false;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _connectToDevice();
-  // }
+  // Future<void> _disconnectFromDevice() async {
+  //   // if (!_isConnected || widget.device == null) {
+  //   //   print('Not connected to a device');
+  //   //   return;
+  //   // }
   //
-  // Future<void> _connectToDevice() async {
-  //   // Connect to the device
-  //   try {
-  //     await widget.device.connect();
-  //     setState(() {
-  //       _isConnected = true;
-  //       _connectedDevice = widget.device;
-  //     });
-  //   } catch (e) {
-  //     setState(() {
-  //       _isConnected = false;
-  //       _connectedDevice = null;
-  //     });
-  //     print('Error connecting to device: $e');
-  //   }
+  //   await widget.device.disconnect();
+  //   print('Disconnected from ${widget.device.name}');
+  //
+  //   setState(() {
+  //     _isDisconnected = false;
+  //     // widget.device = null;
+  //   });
   // }
 
-  // void _disconnectFromDevice() async {
-  //   if (_isConnected && _connectedDevice != null) {
-  //     try {
-  //       await _connectedDevice!.disconnect();
-  //       setState(() {
-  //         _isConnected = false;
-  //         _connectedDevice = null;
-  //       });
-  //     } catch (e) {
-  //       print('Error disconnecting from device: $e');
-  //     }
-  //   }
-  // }
-  Future<void> _disconnectFromDevice() async {
-    // if (!_isConnected || widget.device == null) {
-    //   print('Not connected to a device');
-    //   return;
-    // }
-
-    await widget.device.disconnect();
-    print('Disconnected from ${widget.device.name}');
+  void _disconnectFromDevice() {
+    // Check if the device is connected or perform any necessary checks
+    // ...
 
     setState(() {
-      _isConnected = false;
-      // widget.device = null;
+      _isDisconnected = true;
     });
+
+    // Perform the disconnection logic asynchronously
+    _performDisconnection();
+  }
+
+  Future<void> _performDisconnection() async {
+    await widget.device.disconnect();
+    print('Disconnected from ${widget.device.name}');
   }
 
   @override
@@ -92,13 +73,9 @@ class _GoToRideState extends State<GoToRide> {
             ),
           ),
           actions: [
-            // IconButton(
-            //   icon: Icon(Icons.bluetooth_connected),
-            //   onPressed: _disconnectFromDevice,
-            // ),
             SizedBox(height: 1),
             GlowingButton2(
-              text: "Disconnect",
+              text: _isDisconnected ? "Disconnected" : "Disconnect",
               onPressed: _disconnectFromDevice,
               color1: Color(0xFF2580B3),
               color2: Color(0xFF2580B3),
