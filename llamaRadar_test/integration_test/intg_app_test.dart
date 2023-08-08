83f205077dd2cdd82a5d6090c689bf25979671fb
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-08-08 19:25:46 +0600

    test pair

--- llamaRadar_test/integration_test/intg_app_test.dart
+++ llamaRadar_test/integration_test/intg_app_test.dart
@@ -8,6 +8,7 @@ import 'package:utllama/main.dart' as app;
 import 'package:utllama/temp/glowing_button.dart';
 import 'package:utllama/mode/bleScreen.dart' as newapp;
 import 'package:utllama/mode/goToRide.dart' as rideapp;
+import 'package:utllama/temp/CollisionWarningPage2.dart' as collisionapp;
 import 'package:utllama/temp/CollisionWarningPage2.dart';
 import 'package:utllama/sideBar.dart';
 
@@ -52,8 +53,67 @@ void main() {
   //   expect(find.byType(GoToRide), findsOneWidget);
   // });
 
-  testWidgets('Test Go to Ride page', (WidgetTester tester) async {
+  // testWidgets('Test Go to Ride page', (WidgetTester tester) async {
+  //   // BluetoothDevice? device;
+  //   final device = MockBluetoothDevice();
+  //   app.main();
+  //
+  //   // Delay for a short period to allow the app to render
+  //   await tester.pump(Duration(seconds: 2));
+  //
+  //   final findButton = find.byKey(Key("PairDevice"));
+  //
+  //   await tester.tap(findButton);
+  //   await tester.pumpAndSettle();
+  //
+  //   // Delay for a short period to simulate the scan duration
+  //   await tester.pump(Duration(seconds: 3)); // Increase the duration if needed
+  //
+  //   // Verify that the list of available devices is shown
+  //   expect(find.byType(ListView), findsOneWidget);
+  //
+  //   // Delay to allow navigation to complete
+  //   await tester.pumpAndSettle();
+  //
+  //   final connectButton = find.text('Connect');
+  //
+  //   // Tap the "Connect" button
+  //   await tester.tap(connectButton);
+  //   await tester.pumpAndSettle();
+  //
+  //   // final device = MockBluetoothDevice();
+  //
+  //   // Build the app
+  //   await tester.pumpWidget(
+  //     MaterialApp(
+  //       home: rideapp.GoToRide(device: device),
+  //     ),
+  //   );
+  //
+  //   // Delay for a short period to allow the app to render
+  //   await tester.pumpAndSettle();
+  //
+  //   final findButton2 = find.byKey(Key("Turn on wifi & Go to ride"));
+  //
+  //   await tester.tap(findButton2);
+  //   await tester.pumpAndSettle();
+  //
+  //   await tester.pumpWidget(
+  //     MaterialApp(
+  //       home: collisionapp.CollisionWarningPage2(device: device),
+  //     ),
+  //   );
+  //   await tester.pumpAndSettle();
+  //   // Delay for a short period to simulate the scan duration
+  //   await tester.pump(Duration(seconds: 3)); // Increase the duration if needed
+  //
+  //   // Verify that the ElevatedButton is shown
+  //   expect(find.byKey(Key("CameraButton")), findsOneWidget);
+  // });
 
+  // Another TRY TO PAIR AND NOTIFICATION PAGE
+  testWidgets('Test Go to Ride page', (WidgetTester tester) async {
+    // final device = MockBluetoothDevice();
     app.main();
 
     // Delay for a short period to allow the app to render
@@ -74,7 +134,6 @@ void main() {
     await tester.pumpAndSettle();
 
     final connectButton = find.text('Connect');
-
     // Tap the "Connect" button
     await tester.tap(connectButton);
     await tester.pumpAndSettle();
@@ -88,18 +147,50 @@ void main() {
       ),
     );
 
-    // Delay for a short period to allow the app to render
-    await tester.pumpAndSettle();
+    expect(find.byType(rideapp.GoToRide), findsOneWidget);
 
     final findButton2 = find.byKey(Key("Turn on wifi & Go to ride"));
+    await tester.pump(Duration(seconds: 3));
 
     await tester.tap(findButton2);
     await tester.pumpAndSettle();
 
-    // Delay for a short period to simulate the scan duration
-    await tester.pump(Duration(seconds: 3)); // Increase the duration if needed
+    // Build the app
+    await tester.pumpWidget(
+      MaterialApp(
+        home: collisionapp.CollisionWarningPage2(device: device),
+      ),
+    );
 
-    // Verify that the ElevatedButton is shown
-    expect(find.byKey(Key("CameraButton")), findsOneWidget);
+    expect(find.byType(collisionapp.CollisionWarningPage2), findsOneWidget);
+
+    // final device = MockBluetoothDevice();
+
+    // Build the app
+    // await tester.pumpWidget(
+    //   MaterialApp(
+    //     home: rideapp.GoToRide(device: device),
+    //   ),
+    // );
+    //
+    // // Delay for a short period to allow the app to render
+    // await tester.pumpAndSettle();
+    //
+    // final findButton2 = find.byKey(Key("Turn on wifi & Go to ride"));
+    //
+    // await tester.tap(findButton2);
+    // await tester.pumpAndSettle();
+    //
+    // await tester.pumpWidget(
+    //   MaterialApp(
+    //     home: collisionapp.CollisionWarningPage2(device: device),
+    //   ),
+    // );
+    // await tester.pumpAndSettle();
+    // // Delay for a short period to simulate the scan duration
+    // await tester.pump(Duration(seconds: 3)); // Increase the duration if needed
+    //
+    // // Verify that the ElevatedButton is shown
+    // expect(find.byKey(Key("CameraButton")), findsOneWidget);
   });
 }
