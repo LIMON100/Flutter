f8950ab3e82cf2f6a9d960446287967743a025e9
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-08-09 19:26:37 +0600

    testch

--- llamaRadar_test/integration_test/intg_app_test.dart
+++ llamaRadar_test/integration_test/intg_app_test.dart
@@ -1,61 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_iot_wifi/flutter_iot_wifi.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:utllama/mode/LlamaDefenderPage.dart';
 import 'package:utllama/mode/dash_cam.dart';
 import 'package:utllama/main.dart' as app;
+import 'package:utllama/temp/ConnectWifiForDashCam.dart';
 import 'package:utllama/temp/glowing_button.dart';
 import 'package:utllama/mode/bleScreen.dart' as newapp;
 import 'package:utllama/mode/goToRide.dart' as rideapp;
 import 'package:utllama/temp/CollisionWarningPage2.dart' as collisionapp;
 import 'package:utllama/temp/CollisionWarningPage2.dart';
 import 'package:utllama/sideBar.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 import '../test/radar_test.dart';
+
+class MockMethodChannel extends Mock implements MethodChannel {}
+
+class WifiService {
+  Future<bool?> scanWifi() async {
+    return await FlutterIotWifi.scan();
+  }
+}
 
 void main() {
 
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  // BLUETOOTH PAIR BUTTON
-  // testWidgets('Test Bluetooth Device Pairing', (WidgetTester tester) async {
-  //   // Build the app
-  //   app.main();
-  //
-  //   // Delay for a short period to allow the app to render
-  //   await tester.pump(Duration(seconds: 2));
-  //
-  //   final findButton = find.byKey(Key("PairDevice"));
-  //
-  //   await tester.tap(findButton);
-  //   await tester.pumpAndSettle();
-  //
-  //   // Delay for a short period to simulate the scan duration
-  //   await tester.pump(Duration(seconds: 3)); // Increase the duration if needed
-  //
-  //   // Verify that the list of available devices is shown
-  //   expect(find.byType(ListView), findsOneWidget);
-  //
-  //   // Delay to allow navigation to complete
-  //   await tester.pumpAndSettle();
-  //
-  //   final connectButton = find.text('Connect');
-  //
-  //   // Tap the "Connect" button
-  //   await tester.tap(connectButton);
-  //   await tester.pumpAndSettle();
-  //
-  //   // Delay to allow navigation to GoToRide page
-  //   await tester.pumpAndSettle();
-  //
-  //   // Verify that the app navigated to the GoToRide page
-  //   expect(find.byType(GoToRide), findsOneWidget);
-  // });
-
+  // PAIR AND NOTIFICATION PAGE
   // testWidgets('Test Go to Ride page', (WidgetTester tester) async {
-  //   // BluetoothDevice? device;
-  //   final device = MockBluetoothDevice();
+  //   // final device = MockBluetoothDevice();
   //   app.main();
   //
   //   // Delay for a short period to allow the app to render
@@ -76,121 +54,117 @@ void main() {
   //   await tester.pumpAndSettle();
   //
   //   final connectButton = find.text('Connect');
-  //
   //   // Tap the "Connect" button
   //   await tester.tap(connectButton);
   //   await tester.pumpAndSettle();
   //
-  //   // final device = MockBluetoothDevice();
+  //   // expect(find.text("Turn on wifi & Go to ride"), findsOneWidget);
   //
-  //   // Build the app
-  //   await tester.pumpWidget(
-  //     MaterialApp(
-  //       home: rideapp.GoToRide(device: device),
-  //     ),
-  //   );
-  //
-  //   // Delay for a short period to allow the app to render
-  //   await tester.pumpAndSettle();
-  //
-  //   final findButton2 = find.byKey(Key("Turn on wifi & Go to ride"));
+  //   await tester.pump(Duration(seconds: 2));
+  //   final findButton2 = find.byKey(Key("TurnOnWifiButton"));
   //
   //   await tester.tap(findButton2);
   //   await tester.pumpAndSettle();
   //
-  //   await tester.pumpWidget(
-  //     MaterialApp(
-  //       home: collisionapp.CollisionWarningPage2(device: device),
-  //     ),
-  //   );
-  //   await tester.pumpAndSettle();
-  //   // Delay for a short period to simulate the scan duration
-  //   await tester.pump(Duration(seconds: 3)); // Increase the duration if needed
+  //   expect(collisionapp.CollisionWarningPage2, findsOneWidget);
   //
-  //   // Verify that the ElevatedButton is shown
-  //   expect(find.byKey(Key("CameraButton")), findsOneWidget);
+  //
+  //   // final device = MockBluetoothDevice();
+  //   //
+  //   // // Build the app
+  //   // await tester.pumpWidget(
+  //   //   MaterialApp(
+  //   //     home: rideapp.GoToRide(device: device),
+  //   //   ),
+  //   // );
+  //   //
+  //   // expect(find.byType(rideapp.GoToRide), findsOneWidget);
+  //   //
+  //   // final findButton2 = find.byKey(Key("Turn on wifi & Go to ride"));
+  //   // await tester.pump(Duration(seconds: 3));
+  //   //
+  //   // await tester.tap(findButton2);
+  //   // await tester.pumpAndSettle();
+  //
+  //   // Build the app
+  //   // await tester.pumpWidget(
+  //   //   MaterialApp(
+  //   //     home: collisionapp.CollisionWarningPage2(device: device),
+  //   //   ),
+  //   // );
+  //   //
+  //   // expect(find.byType(collisionapp.CollisionWarningPage2), findsOneWidget);
   // });
 
-  // Another TRY TO PAIR AND NOTIFICATION PAGE
-  testWidgets('Test Go to Ride page', (WidgetTester tester) async {
-    // final device = MockBluetoothDevice();
-    app.main();
+  // testWidgets('Test ConnectWifiForDashCam', (WidgetTester tester) async {
+  //   // Build the app
+  //   await tester.pumpWidget(
+  //     MaterialApp(
+  //       home: ConnectWifiForDashCam(),
+  //     ),
+  //   );
+  //
+  //   // Wait for the initial animations and transitions to complete
+  //   await tester.pumpAndSettle();
+  //
+  //   // Tap the "Connect Wifi" button
+  //   final connectWifiButton = find.byIcon(Icons.wifi);
+  //   await tester.tap(connectWifiButton);
+  //
+  //   // Wait for the dialog to appear
+  //   await tester.pumpAndSettle();
+  //   await tester.pump(Duration(seconds: 5));
+  //
+  //   // Find and tap the Wi-Fi network that starts with "Mahmudur"
+  //   final wifiNetwork = find.widgetWithText(showDialog, "Mahmudur");
+  //   await tester.tap(wifiNetwork);
+  //
+  //   // Wait for the Wi-Fi connection to complete
+  //   await tester.pumpAndSettle();
+  //
+  //   // Verify the connection status based on the isConnected variable
+  //   // final isConnected = tester.state<ConnectWifiForDashCamState>(
+  //   //   find.byType(ConnectWifiForDashCam),
+  //   // ).isConnected;
+  //   //
+  //   // // Assert the connectivity status
+  //   // expect(isConnected, isTrue); // Use isFalse if you expect disconnection
+  //
+  // });
 
-    // Delay for a short period to allow the app to render
-    await tester.pump(Duration(seconds: 2));
-
-    final findButton = find.byKey(Key("PairDevice"));
-
-    await tester.tap(findButton);
-    await tester.pumpAndSettle();
-
-    // Delay for a short period to simulate the scan duration
-    await tester.pump(Duration(seconds: 3)); // Increase the duration if needed
-
-    // Verify that the list of available devices is shown
-    expect(find.byType(ListView), findsOneWidget);
-
-    // Delay to allow navigation to complete
-    await tester.pumpAndSettle();
-
-    final connectButton = find.text('Connect');
-    // Tap the "Connect" button
-    await tester.tap(connectButton);
-    await tester.pumpAndSettle();
-
-    final device = MockBluetoothDevice();
-
-    // Build the app
-    await tester.pumpWidget(
-      MaterialApp(
-        home: rideapp.GoToRide(device: device),
-      ),
-    );
-
-    expect(find.byType(rideapp.GoToRide), findsOneWidget);
-
-    final findButton2 = find.byKey(Key("Turn on wifi & Go to ride"));
-    await tester.pump(Duration(seconds: 3));
-
-    await tester.tap(findButton2);
-    await tester.pumpAndSettle();
+  testWidgets('Test _startWifiScan dialog', (WidgetTester tester) async {
+    // Mock FlutterIotWifi method channel
+    final mockWifiService = WifiService();
+    when(mockWifiService.scanWifi()).thenAnswer((_) => Future.value(true));
 
     // Build the app
     await tester.pumpWidget(
       MaterialApp(
-        home: collisionapp.CollisionWarningPage2(device: device),
+        home: ConnectWifiForDashCam(wifiService: mockWifiService),
       ),
     );
 
-    expect(find.byType(collisionapp.CollisionWarningPage2), findsOneWidget);
+    // Wait for the initial animations and transitions to complete
+    await tester.pumpAndSettle();
 
-    // final device = MockBluetoothDevice();
+    // Mock successful Wi-Fi scan result
+    mockMethodChannel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'scan') {
+        return true;
+      }
+      return null;
+    });
 
-    // Build the app
-    // await tester.pumpWidget(
-    //   MaterialApp(
-    //     home: rideapp.GoToRide(device: device),
-    //   ),
-    // );
-    //
-    // // Delay for a short period to allow the app to render
-    // await tester.pumpAndSettle();
-    //
-    // final findButton2 = find.byKey(Key("Turn on wifi & Go to ride"));
-    //
-    // await tester.tap(findButton2);
-    // await tester.pumpAndSettle();
-    //
-    // await tester.pumpWidget(
-    //   MaterialApp(
-    //     home: collisionapp.CollisionWarningPage2(device: device),
-    //   ),
-    // );
-    // await tester.pumpAndSettle();
-    // // Delay for a short period to simulate the scan duration
-    // await tester.pump(Duration(seconds: 3)); // Increase the duration if needed
-    //
-    // // Verify that the ElevatedButton is shown
-    // expect(find.byKey(Key("CameraButton")), findsOneWidget);
+    // Trigger _startWifiScan
+    final connectWifiButton = find.byIcon(Icons.wifi);
+    await tester.tap(connectWifiButton);
+    await tester.pumpAndSettle();
+
+    // Verify that the dialog is displayed
+    expect(find.byType(Dialog), findsOneWidget);
+
+    // You can further test the dialog content and interactions here
+    // For example, you can tap on a Wi-Fi network and verify that _connect is called
   });
+
 }
