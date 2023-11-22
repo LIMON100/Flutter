388fb24a1ece0a1ba41e8392795c45147af18e52
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-11-22 16:54:50 +0600

    Add files via upload
    updat eboth showandtestdata

--- Registration_Login_page/lib/final/showGpsData.dart
+++ Registration_Login_page/lib/final/showGpsData.dart
@@ -1,14 +1,7 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:testgpss/final/Screens/LogInScreen.dart';
-import 'package:testgpss/temp/LocationPageSharedPreference.dart';
-
-import '../final/model/UserLogInStatus.dart';
+import 'package:testgpss/final/temp/DateDetailsPage.dart';
 import '../final/sqflite/sqlite.dart';
 
 class ShowGpsData extends StatefulWidget {
@@ -17,19 +10,178 @@ class ShowGpsData extends StatefulWidget {
   @override
   State<ShowGpsData> createState() => _ShowGpsDataState();
 }
+//
+// class _ShowGpsDataState extends State<ShowGpsData> {
+//   List<Map<String, dynamic>> _gpsCoordinates = [];
+//
+//   Future<void> _fetchGpsCoordinates() async {
+//     try {
+//       final Database db = await GpsDatabaseHelper().initDatabase();
+//       final List<Map<String, dynamic>> gpsCoordinates = await db.query('gps_coordinates');
+//
+//       setState(() {
+//         _gpsCoordinates = gpsCoordinates;
+//       });
+//     } catch (e) {
+//       print('Error fetching GPS coordinates: $e');
+//     }
+//   }
+//
+//
+//   Map<String, List<Map<String, dynamic>>> _groupedGpsCoordinates = {};
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _fetchGpsCoordinates();
+//     _groupedGpsCoordinates = _groupDataByDate(_gpsCoordinates);
+//   }
+//
+//   Map<String, List<Map<String, dynamic>>> _groupDataByDate(
+//       List<Map<String, dynamic>> data) {
+//     Map<String, List<Map<String, dynamic>>> groupedData = {};
+//
+//     for (var item in data) {
+//       String date = item['date'];
+//       groupedData[date] ??= [];
+//       groupedData[date]!.add(item);
+//     }
+//
+//     return groupedData;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('GPS Coordinates'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: _groupedGpsCoordinates.keys.length,
+//         itemBuilder: (context, dateIndex) {
+//           String date = _groupedGpsCoordinates.keys.elementAt(dateIndex);
+//
+//           return ListTile(
+//             title: Text(date),
+//             onTap: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) =>
+//                       DateDetailsPage(date: date, details: _groupedGpsCoordinates[date]!),
+//                 ),
+//               );
+//             },
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+
+// class _ShowGpsDataState extends State<ShowGpsData> {
+//   List<Map<String, dynamic>> _gpsCoordinates = [];
+//
+//   Future<void> _fetchGpsCoordinates() async {
+//     final Database db = await GpsDatabaseHelper().initDatabase();
+//     final List<Map<String, dynamic>> gpsCoordinates =
+//     await db.query('gps_coordinates', orderBy: 'date DESC'); // Sort by date in descending order
+//
+//     // Create a set to store unique dates
+//     Set<String> uniqueDates = Set();
+//
+//     // Filter out duplicates based on date
+//     List<Map<String, dynamic>> uniqueGpsCoordinates = [];
+//     for (var gpsCoordinate in gpsCoordinates) {
+//       if (uniqueDates.add(gpsCoordinate['date'])) {
+//         uniqueGpsCoordinates.add(gpsCoordinate);
+//       }
+//     }
+//
+//     setState(() {
+//       _gpsCoordinates = uniqueGpsCoordinates;
+//     });
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _fetchGpsCoordinates();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('GPS Coordinates'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: _gpsCoordinates.length,
+//         itemBuilder: (context, index) {
+//           final gpsCoordinate = _gpsCoordinates[index];
+//           return GestureDetector(
+//             onTap: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => DateDetailsPage(
+//                     date: gpsCoordinate['date'],
+//                     details: _gpsCoordinates,
+//                   ),
+//                 ),
+//               );
+//             },
+//             child: ListTile(
+//               title: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Text(
+//                     '${gpsCoordinate['date']}',
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
 
 class _ShowGpsDataState extends State<ShowGpsData> {
-
   List<Map<String, dynamic>> _gpsCoordinates = [];
 
   Future<void> _fetchGpsCoordinates() async {
     final Database db = await GpsDatabaseHelper().initDatabase();
     final List<Map<String, dynamic>> gpsCoordinates =
-    await db.query('gps_coordinates');
+    await db.query('gps_coordinates', orderBy: 'date DESC');
+
+    // Use a set to store unique dates
+    Set<String> uniqueDates = Set();
+
+    // Filter out duplicates based on date
+    List<Map<String, dynamic>> uniqueGpsCoordinates = [];
+    for (var gpsCoordinate in gpsCoordinates) {
+      if (uniqueDates.add(gpsCoordinate['date'])) {
+        uniqueGpsCoordinates.add(gpsCoordinate);
+      }
+    }
 
     setState(() {
-      _gpsCoordinates = gpsCoordinates;
+      _gpsCoordinates = uniqueGpsCoordinates;
     });
+  }
+
+  Future<List<String>?> getFilesByDate(String date) async {
+    final database = await GpsDatabaseHelper().initDatabase();
+    final results = await database.query('gps_coordinates',
+      where: 'date == ?',
+      whereArgs: [date],
+    );
+
+    final files = results.map((row) => row['image']).toList();
   }
 
   @override
@@ -48,26 +200,27 @@ class _ShowGpsDataState extends State<ShowGpsData> {
         itemCount: _gpsCoordinates.length,
         itemBuilder: (context, index) {
           final gpsCoordinate = _gpsCoordinates[index];
-
-          return ListTile(
-            title: Text(
-              '${gpsCoordinate['latitude']}, ${gpsCoordinate['longitude']}',
-            ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Date: ${gpsCoordinate['date']} Time: ${gpsCoordinate['time']}',
-                ),
-                SizedBox(height: 8.0),
-                // Display the image if it exists
-                if (gpsCoordinate['image'] != null)
-                  Image.asset(
-                    gpsCoordinate['image'], // use the asset name directly
-                    height: 100.0,
-                    width: 100.0,
+          return GestureDetector(
+            // onTap:()=> getFilesByDate,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DateDetailsPage(
+                    date: gpsCoordinate['date'],
                   ),
-              ],
+                ),
+              );
+            },
+            child: ListTile(
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${gpsCoordinate['date']}',
+                  ),
+                ],
+              ),
             ),
           );
         },
@@ -75,3 +228,4 @@ class _ShowGpsDataState extends State<ShowGpsData> {
     );
   }
 }
+
--- Registration_Login_page/lib/final/testGpscordData.dart
+++ Registration_Login_page/lib/final/testGpscordData.dart
@@ -20,47 +20,197 @@ class TestGpscordData extends StatefulWidget {
 }
 
 class _TestGpscordDataState extends State<TestGpscordData> {
-  List<Map<String, dynamic>> demoData = [
-    {
-      "latitude": 34.710648,
-      "longitude": 800.406969,
-      "image": "assets/images/logo.png",
-      "date": "2023-11-20",
-      "time": "16:26"
-    },
-    {
-      "latitude": 83.710648,
-      "longitude": 0.406969,
-      "image": "assets/images/land_tree_dark.png",
-      "date": "2023-11-20",
-      "time": "16:26"
-    },
-    {
-      "latitude": 23.710648,
-      "longitude": 800.406969,
-      "image": "assets/images/land_tree_dark.png",
-      "date": "2023-11-20",
-      "time": "16:26"
-    },
-    {
-      "latitude": 03.710648,
-      "longitude": 90.406969,
-      "image": "assets/images/logo.png",
-      "date": "2023-11-20",
-      "time": "16:26"
-    },
-    {
-      "latitude": 1.710648,
-      "longitude": 2.406969,
-      "image": "assets/images/land_tree_light.png",
-      "date": "2023-11-20",
-      "time": "16:26"
-    },
-  ];
+
+  String? _currentAddress;
+  Position? _currentPosition;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    // _loadSavedCoordinates();
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      _getCurrentPosition();
+    });
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+
+    if (!hasPermission) return;
+
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+      _getAddressFromLatLng(_currentPosition!);
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
+
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getAddressFromLatLng(Position position) async {
+    await placemarkFromCoordinates(
+        _currentPosition!.latitude, _currentPosition!.longitude)
+        .then((List<Placemark> placemarks) {
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress =
+        '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
+      });
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
+
+
+  List<Map<String, dynamic>> getDemoData() {
+    return [
+      {
+        "latitude": _currentPosition?.latitude,
+        "longitude": _currentPosition?.longitude,
+        "image": "assets/images/logo.png",
+        "date": "2023-11-11",
+        "time": "16:26",
+      },
+      {
+        "latitude": _currentPosition?.latitude,
+        "longitude": _currentPosition?.longitude,
+        "image": "assets/images/logo.png",
+        "date": "2023-11-11",
+        "time": "16:26",
+      },
+      {
+        "latitude": 83.710648,
+        "longitude": 0.406969,
+        "image": "assets/images/land_tree_dark.png",
+        "date": "2023-11-11",
+        "time": "16:26"
+      },
+      {
+        "latitude": 23.710648,
+        "longitude": 800.406969,
+        "image": "assets/images/land_tree_dark.png",
+        "date": "2023-11-21",
+        "time": "16:26"
+      },
+      {
+        "latitude": 03.710648,
+        "longitude": 90.406969,
+        "image": "assets/images/logo.png",
+        "date": "2023-11-10",
+        "time": "16:26"
+      },
+      {
+        "latitude": 1.710648,
+        "longitude": 2.406969,
+        "image": "assets/images/land_tree_light.png",
+        "date": "2023-11-05",
+        "time": "16:26"
+      },
+      {
+        "latitude": 1232.710648,
+        "longitude": 22.406969,
+        "image": "assets/images/land_tree_light.png",
+        "date": "2023-11-05",
+        "time": "16:26"
+      },
+      {
+        "latitude": 0.710648,
+        "longitude": 02.406969,
+        "image": "assets/images/land_tree_light.png",
+        "date": "2023-09-05",
+        "time": "16:26"
+      },
+    ];
+  }
+
+  // List<Map<String, dynamic>> demoData = [
+  //   {
+  //     "latitude": _currentPosition!.latitude,
+  //     "longitude": _currentPosition!.longitude,
+  //     "image": "assets/images/logo.png",
+  //     "date": "2023-11-11",
+  //     "time": "16:26"
+  //   },
+  //   {
+  //     "latitude": 83.710648,
+  //     "longitude": 0.406969,
+  //     "image": "assets/images/land_tree_dark.png",
+  //     "date": "2023-11-11",
+  //     "time": "16:26"
+  //   },
+  //   {
+  //     "latitude": 23.710648,
+  //     "longitude": 800.406969,
+  //     "image": "assets/images/land_tree_dark.png",
+  //     "date": "2023-11-21",
+  //     "time": "16:26"
+  //   },
+  //   {
+  //     "latitude": 03.710648,
+  //     "longitude": 90.406969,
+  //     "image": "assets/images/logo.png",
+  //     "date": "2023-11-10",
+  //     "time": "16:26"
+  //   },
+  //   {
+  //     "latitude": 1.710648,
+  //     "longitude": 2.406969,
+  //     "image": "assets/images/land_tree_light.png",
+  //     "date": "2023-11-05",
+  //     "time": "16:26"
+  //   },
+  //   {
+  //     "latitude": 1232.710648,
+  //     "longitude": 22.406969,
+  //     "image": "assets/images/land_tree_light.png",
+  //     "date": "2023-11-05",
+  //     "time": "16:26"
+  //   },
+  //   {
+  //     "latitude": 0.710648,
+  //     "longitude": 02.406969,
+  //     "image": "assets/images/land_tree_light.png",
+  //     "date": "2023-09-05",
+  //     "time": "16:26"
+  //   },
+  // ];
 
   GpsDatabaseHelper helper = GpsDatabaseHelper();
   Future<void> insertDemoData() async {
-    for(Map<String, dynamic> row in demoData) {
+    for(Map<String, dynamic> row in getDemoData()) {
       await helper.insertCoordinates(row);
     }
   }
